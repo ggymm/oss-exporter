4b5fbdb2c8a3cfebffa3e07add502a643826a2f2
gongym <1967988842@qq.com>
2021-10-18 16:30:27 +0800

    提交代码

--- dell.go
+++ dell.go
@@ -6,11 +6,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/buger/jsonparser"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -36,6 +37,22 @@ type DellRpcRequest struct {
 }
 
 type DellCrawlerData struct {
+	UsedSpace  map[string]string `json:"usedSpace"`
+	FreeSpace  map[string]string `json:"freeSpace"`
+	TotalSpace map[string]string `json:"totalSpace"`
+
+	PoolUsedSpace  map[string]string `json:"poolUsedSpace"`
+	PoolFreeSpace  map[string]string `json:"poolFreeSpace"`
+	PoolTotalSpace map[string]string `json:"poolTotalSpace"`
+
+	EnclosureInfo []map[string]string `json:"enclosureInfo"`
+	DiskInfo      []map[string]string `json:"diskInfo"`
+	PortInfo      []map[string]string `json:"portInfo"`
+}
+
+func (h *DellCrawlerData) PrintStr() {
+	data, _ := json.MarshalIndent(&h, "", "  ")
+	fmt.Println(string(data))
 }
 
 type Dell struct {
@@ -52,6 +69,8 @@ type Dell struct {
 
 	SerialNumber string
 
+	wg          sync.WaitGroup
+	wsConn      *websocket.Conn
 	CrawlerData *DellCrawlerData
 }
 
@@ -109,9 +128,16 @@ func (c *Dell) Start() {
 		}
 	}
 
-	fmt.Println(c.AuthCookie)
-	c.Connect()
 	// 启动WebSocket客户端
+	if err := c.Connect(); err != nil {
+		return
+	}
+	// 获取基础信息
+	c.GetBasicInfo()
+	// 获取硬盘信息、
+	c.GetDiskInfo()
+
+	c.CrawlerData.PrintStr()
 }
 
 func (c *Dell) Login() error {
@@ -197,7 +223,7 @@ func (c *Dell) Login() error {
 	}
 }
 
-func (c *Dell) Connect() {
+func (c *Dell) Connect() error {
 	// 创建websocket客户端
 	dialer := websocket.Dialer{
 		TLSClientConfig: &tls.Config{
@@ -205,25 +231,74 @@ func (c *Dell) Connect() {
 			InsecureSkipVerify: true,
 		},
 	}
-	wsConn, _, err := dialer.Dial(c.WSHost+"/messages", http.Header{
+	var err error
+	c.wsConn, _, err = dialer.Dial(c.WSHost+"/messages", http.Header{
 		"Cookie": []string{c.AuthCookie},
 	})
-	if err != nil || wsConn == nil {
-		return
+	if err != nil || c.wsConn == nil {
+		return err
+	} else {
+		return nil
 	}
+}
 
-	// 同步等待所有异步任务结束
-	var wg sync.WaitGroup
-
+func (c *Dell) GetBasicInfo() {
 	go func() {
 		for {
-			_, message, err := wsConn.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
+			if _, message, err := c.wsConn.ReadMessage(); err != nil {
+				c.Log.Errorf("Websocket读取数据失败, message: %b, error: %v", message, err)
 				return
+			} else {
+				correlationId := gjson.Get(string(message), "correlationId").String()
+				switch correlationId {
+				case "1": // 总容量
+					_, _ = jsonparser.ArrayEach(message, func(value []byte, valueType jsonparser.ValueType, offset int, err error) {
+						seriesColorId := gjson.Get(string(value), "seriesColorId").String()
+
+						data := gjson.Get(string(value), "value").String()
+						title := gjson.Get(string(value), "caption").String()
+						if seriesColorId == "UsedSpace" { // 已使用
+							c.CrawlerData.UsedSpace["data"] = data
+							c.CrawlerData.UsedSpace["title"] = title
+						} else if seriesColorId == "FreeSpace" { // 可用
+							c.CrawlerData.FreeSpace["data"] = data
+							c.CrawlerData.FreeSpace["title"] = title
+						}
+					}, "result.chartData")
+
+					c.CrawlerData.TotalSpace["data"] = gjson.Get(string(message), "result.totalSpace.bytes").String()
+					c.CrawlerData.TotalSpace["title"] = gjson.Get(string(message), "result.totalSpace.displayString").String()
+				case "2": // 存储池容量
+					_, _ = jsonparser.ArrayEach(message, func(value []byte, valueType jsonparser.ValueType, offset int, err error) {
+						seriesColorId := gjson.Get(string(value), "seriesColorId").String()
+
+						data := gjson.Get(string(value), "value").String()
+						title := gjson.Get(string(value), "caption").String()
+						if seriesColorId == "UsedSpace" { // 已使用
+							c.CrawlerData.PoolUsedSpace["data"] = data
+							c.CrawlerData.PoolUsedSpace["title"] = title
+						} else if seriesColorId == "FreeSpace" { // 可用
+							c.CrawlerData.PoolFreeSpace["data"] = data
+							c.CrawlerData.PoolFreeSpace["title"] = title
+						}
+					}, "result.sizeChartData")
+
+					c.CrawlerData.PoolTotalSpace["data"] = gjson.Get(string(message), "result.allocatedSpace.bytes").String()
+					c.CrawlerData.PoolTotalSpace["title"] = gjson.Get(string(message), "result.allocatedSpace.displayString").String()
+				case "3": // 机柜状态
+					_, _ = jsonparser.ArrayEach(message, func(value []byte, valueType jsonparser.ValueType, offset int, err error) {
+						enclosureInfo := make(map[string]string)
+						enclosureInfo["name"] = gjson.Get(string(value), "name").String()
+						enclosureInfo["index"] = gjson.Get(string(value), "index").String()
+						enclosureInfo["instanceId"] = gjson.Get(string(value), "instanceId").String()
+						enclosureInfo["status"] = gjson.Get(string(value), "status.enum").String()
+						enclosureInfo["statusName"] = gjson.Get(string(value), "status.enumName").String()
+
+						c.CrawlerData.EnclosureInfo = append(c.CrawlerData.EnclosureInfo, enclosureInfo)
+					}, "result.enclosureList")
+				}
 			}
-			log.Printf("recv: %s", string(message))
-			wg.Done()
+			c.wg.Done()
 		}
 	}()
 
@@ -237,10 +312,10 @@ func (c *Dell) Connect() {
 	getCapacityData.MethodArguments = []string{c.SerialNumber}
 	getCapacityData.HandlerName = "StorageCenterSummaryService"
 	getCapacityDataMsg, _ := json.Marshal(getCapacityData)
-	if err := wsConn.WriteMessage(websocket.TextMessage, getCapacityDataMsg); err != nil {
+	if err := c.wsConn.WriteMessage(websocket.TextMessage, getCapacityDataMsg); err != nil {
 		c.Log.Errorf("获取总容量请求发送失败, error: %v", err)
 	} else {
-		wg.Add(1)
+		c.wg.Add(1)
 	}
 
 	// 存储池容量
@@ -253,27 +328,108 @@ func (c *Dell) Connect() {
 	listStorageTypes.MethodArguments = []string{c.SerialNumber}
 	listStorageTypes.HandlerName = "StorageTypeService"
 	listStorageTypesMsg, _ := json.Marshal(listStorageTypes)
-	if err := wsConn.WriteMessage(websocket.TextMessage, listStorageTypesMsg); err != nil {
+	if err := c.wsConn.WriteMessage(websocket.TextMessage, listStorageTypesMsg); err != nil {
 		c.Log.Errorf("获取存储池容量请求发送失败, error: %v", err)
 	} else {
-		wg.Add(1)
+		c.wg.Add(1)
 	}
+
+	// 机柜状态
+	c.Log.Debug("[RPC]机柜状态")
+	getHardwareOverview := new(DellRpcRequest)
+	getHardwareOverview.Type = "rpc-call"
+	getHardwareOverview.PluginId = "sc"
+	getHardwareOverview.CorrelationId = "3"
+	getHardwareOverview.MethodName = "getHardwareOverview"
+	getHardwareOverview.MethodArguments = []string{c.SerialNumber}
+	getHardwareOverview.HandlerName = "StorageCenterService"
+	getHardwareOverviewMsg, _ := json.Marshal(getHardwareOverview)
+	if err := c.wsConn.WriteMessage(websocket.TextMessage, getHardwareOverviewMsg); err != nil {
+		c.Log.Errorf("获取机柜状态请求发送失败, error: %v", err)
+	} else {
+		c.wg.Add(1)
+	}
+
+	c.wg.Wait()
+}
+
+func (c *Dell) GetDiskInfo() {
+	go func() {
+		for {
+			if _, message, err := c.wsConn.ReadMessage(); err != nil {
+				c.Log.Errorf("Websocket读取数据失败, message: %b, error: %v", message, err)
+				return
+			} else {
+				_, _ = jsonparser.ArrayEach(message, func(value []byte, valueType jsonparser.ValueType, offset int, err error) {
+					diskInfo := make(map[string]string)
+					diskInfo["name"] = gjson.Get(string(value), "name").String()
+					diskInfo["index"] = gjson.Get(string(value), "index").String()
+					diskInfo["instanceId"] = gjson.Get(string(value), "instanceId").String()
+					diskInfo["status"] = gjson.Get(string(value), "status.enum").String()
+					diskInfo["statusName"] = gjson.Get(string(value), "status.enumName").String()
+
+					c.CrawlerData.DiskInfo = append(c.CrawlerData.DiskInfo, diskInfo)
+				}, "result.items")
+			}
+			c.wg.Done()
+		}
+	}()
 
 	// 硬盘状态
-	c.Log.Debug("[RPC]硬盘状态")
-	getHardwareDisks := new(DellRpcRequest)
-	getHardwareDisks.Type = "rpc-call"
-	getHardwareDisks.PluginId = "sc"
-	getHardwareDisks.CorrelationId = "3"
-	getHardwareDisks.MethodName = "getHardwareDisks"
-	getHardwareDisks.MethodArguments = []string{c.SerialNumber}
-	getHardwareDisks.HandlerName = "DiskService"
-	getHardwareDisksMsg, _ := json.Marshal(getHardwareDisks)
-	if err := wsConn.WriteMessage(websocket.TextMessage, getHardwareDisksMsg); err != nil {
-		c.Log.Errorf("获取硬盘状态请求发送失败, error: %v", err)
-	} else {
-		wg.Add(1)
+	for i, info := range c.CrawlerData.EnclosureInfo {
+		c.Log.Debug("[RPC]硬盘状态")
+		getHardwareDisks := new(DellRpcRequest)
+		getHardwareDisks.Type = "rpc-call"
+		getHardwareDisks.PluginId = "sc"
+		getHardwareDisks.CorrelationId = strconv.Itoa(4 + i)
+		getHardwareDisks.MethodName = "getHardwareDisks"
+		getHardwareDisks.MethodArguments = []string{c.SerialNumber, info["index"]}
+		getHardwareDisks.HandlerName = "DiskService"
+		getHardwareDisksMsg, _ := json.Marshal(getHardwareDisks)
+		if err := c.wsConn.WriteMessage(websocket.TextMessage, getHardwareDisksMsg); err != nil {
+			c.Log.Errorf("获取硬盘状态请求发送失败, error: %v", err)
+		} else {
+			c.wg.Add(1)
+		}
 	}
 
-	wg.Wait()
+	c.wg.Wait()
+}
+
+func (c *Dell) GetPortInfo() {
+	go func() {
+		for {
+			if _, message, err := c.wsConn.ReadMessage(); err != nil {
+				c.Log.Errorf("Websocket读取数据失败, message: %b, error: %v", message, err)
+				return
+			} else {
+				_, _ = jsonparser.ArrayEach(message, func(value []byte, valueType jsonparser.ValueType, offset int, err error) {
+					portInfo := make(map[string]string)
+					portInfo["name"] = gjson.Get(string(value), "name").String()
+					portInfo["instanceId"] = gjson.Get(string(value), "instanceId").String()
+					portInfo["status"] = gjson.Get(string(value), "status.enum").String()
+					portInfo["statusName"] = gjson.Get(string(value), "status.enumName").String()
+
+					c.CrawlerData.PortInfo = append(c.CrawlerData.PortInfo, portInfo)
+				}, "result.items")
+			}
+			c.wg.Done()
+		}
+	}()
+
+	// 端口状态
+	c.Log.Debug("[RPC]端口状态")
+	getControllerPorts := new(DellRpcRequest)
+	getControllerPorts.Type = "rpc-call"
+	getControllerPorts.PluginId = "sc"
+	getControllerPorts.CorrelationId = strconv.Itoa(4 + len(c.CrawlerData.EnclosureInfo))
+	getControllerPorts.MethodName = "getControllerPorts"
+	getControllerPorts.MethodArguments = []string{c.SerialNumber}
+	getControllerPorts.HandlerName = "ControllerService"
+	getControllerPortsMsg, _ := json.Marshal(getControllerPorts)
+	if err := c.wsConn.WriteMessage(websocket.TextMessage, getControllerPortsMsg); err != nil {
+		c.Log.Errorf("获取端口状态请求发送失败, error: %v", err)
+	} else {
+		c.wg.Add(1)
+	}
 }
